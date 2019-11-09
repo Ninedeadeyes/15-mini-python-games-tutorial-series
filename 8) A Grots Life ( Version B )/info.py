e21d727dd21c4c20c2fd89cfabf45d0cf79acc87
Ninedeadeyes <tommykwong_84@hotmail.com>
2019-11-09 16:00:25 +0000

    Add files via upload

---
+++ 8) A Grots Life ( Version B )/info.py
@@ -0,0 +1,7 @@
+class List:
+    def __init__(self):
+        self.foodlist=("Happy Mushrooms","Bony Bones","Rotten Apples","Mystery Meat")
+        self.enemylist=("Tiny Tiny Dragon","Beta Male Orc","Hobbit Berserker","Tiny Tiny Dragon","Sucidal Jester","Cannibal Gnome")
+        self.lootlist=("Fish Bone","Dirty Potato Sack","Fist Full of Straws","Wood Stick","Giant Boot","Rusty Umbrella")
+
+ 
