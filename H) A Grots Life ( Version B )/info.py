30ced00e5caf07df774c08f2b6cf64af429f7cb2
Ninedeadeyes <tommykwong_84@hotmail.com>
2020-02-05 19:28:56 +0000

    Delete info.py

--- H) A Grots Life ( Version B )/info.py
+++
@@ -1,6 +0,0 @@
-
-foodlist=("Happy Mushrooms","Bony Bones","Rotten Apples","Mystery Meat")
-enemylist=("Tiny Tiny Dragon","Beta Male Orc","Hobbit Berserker","Tiny Tiny Dragon","Sucidal Jester","Cannibal Gnome")
-lootlist=("Fish Bone","Dirty Potato Sack","Fist Full of Straws","Wood Stick","Giant Boot","Rusty Umbrella")
-
- 
