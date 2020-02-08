220ffe2ceb2048aa11de83d2f376b06f86ea6d13
Ninedeadeyes <tommykwong_84@hotmail.com>
2020-02-08 21:37:53 +0000

    Add files via upload

---
+++ D) Battle Master.py
@@ -0,0 +1,127 @@
+import time, random
+counter=0
+player1=0
+player2=0
+
+
+def player1_win():
+    print("Orcs win the battle")
+    global player1
+    player1+=1
+
+def player2_win():
+    print("Humans win the battle")
+    global player2
+    player2+=1
+
+
+while True:
+
+    print("""The war between Orcs and Humans rage on forever
+    "You are the new commander, you have 100 men which you have to
+    allocate to the apprioate position to win the war """)
+
+  
+    while True:  
+        knight=input("how many Knights ?")
+        try:
+            int(knight)
+            break
+
+        except ValueError:
+            print("This is not a number")
+
+
+    while True: 
+        farmer=input("how many Farmers ?")
+        try:
+            int(farmer)
+            break
+
+        except ValueError:
+            print("This is not a number")
+                
+
+    while True:
+        defender=input("how many Defenders ?")
+        try:
+            int(defender)
+            break
+
+        except ValueError:
+            print("This is not a number")
+
+
+    
+    tog=int(defender)+int(farmer)+int(knight)
+    
+    if tog>100:
+        print("You have allocated over 100")
+
+    else:
+        print("You have",str(defender),"Defenders.You have",str(farmer),"Farmers and"
+              ,str(knight),"Knights")
+        break 
+
+            
+    
+     
+
+
+while True:
+    
+
+    while True:
+        counter+=1
+        print("The battle rages on and on ..")
+        time.sleep(.5)
+        print("...")
+        time.sleep(.5)
+        print("...")
+        time.sleep(.5)
+        print("...")
+
+        if counter==3:
+            break
+
+    print("A victor has been decided")
+
+    x=random.randint(1,3)
+
+    time.sleep(x)
+
+    defender1=int(defender)+random.randint(1,15)
+    farmer1=int(farmer)+random.randint(1,10)
+    knight1=int(knight)+random.randint(1,15)
+
+    
+
+    if defender1>=38 and farmer1>=15 and knight1>=55:
+        player2_win()
+
+    else:
+        player1_win()
+
+    print("Orc victory:"+str(player1)+" Human victory:"+str(player2))
+    counter=0
+
+    if player1==5:
+        print("""The Orcs has won the war. You have been defeated !!""")
+        break
+        
+
+    if player2==5:
+        print("The Humans has won the war, congratulations !!")
+        break
+        
+        
+
+
+
+  
+
+        
+
+
+
+    
---
+++ E) Blood Pit .py
@@ -0,0 +1,121 @@
+hero_max_power=20
+hero_max_health=300
+hero_health=[300]
+hero_power=[20]
+turn_counter=0
+rep=0
+
+def enemy(name,a,b):
+    print(name+" "+str(a)+"hp "+str(b)+" power")
+    health=a
+    power=b  
+    hh= hero_health[0]
+    hp= hero_power[0]
+    while hh>0 or health>0:
+
+        (hh)-=(power)
+        (health)-=(hp)
+        print("The "+name+" attack you.Your health is "+str(hh))
+        print("You attack the "+name+".The "+name+" health is "+ str(health))
+        
+        if hh<=0:
+            print("hero has been slained") 
+            input("Press enter to quit")
+            quit()
+        
+        if health<=0:
+            print("You have slained the "+name)
+            break
+        
+            
+        
+
+
+while True:
+    
+    print("Welcome to the blood pit, choose your opponent!! ")
+    choose1=input( "Please select Goblin, Orc, Pit Fighter or Red Dragon: ")
+    choose=choose1.upper()
+
+    while choose is "GOBLIN" or "ORC" or "PIT FIGHTER" or "RED DRAGON":
+
+        if choose==("GOBLIN"):
+            print("You fight")
+            enemy("Goblin",200,20)
+            rep+=10
+            hero_max_health+=20
+            hero_health[0]=hero_max_health
+            hero_max_power+=5
+            hero_power[0]=hero_max_power
+            print("you become stronger with every goblin you slay")
+            print("you gain some reputation for the fight")
+            print("Totol Reputation: "+ str(rep))
+            break
+            
+
+
+        elif choose==("ORC"):
+            print("You fight")
+            enemy("Orc",400,60)
+            rep+=50
+            hero_max_health+=40
+            hero_health[0]=hero_max_health
+            hero_max_power+=10
+            hero_power[0]=hero_max_power      
+            print("you become more powerful with every orc you crush")
+            print("you gain some reputation for the fight")
+            print("Totol Reputation: "+ str(rep))
+            break
+        
+
+        elif choose==("PIT FIGHTER"):
+            print("You fight")
+            enemy("Pit Fighter",500,200)
+            rep+=100
+            hero_max_health+=60
+            hero_health[0]=hero_max_health
+            hero_max_power+=20
+            hero_power[0]=hero_max_power       
+            print("you gain experience for every pit fighter you battle")
+            print("you gain some reputation for the fight")
+            print("Totol Reputation: "+ str(rep))
+            break
+
+        elif choose==("RED DRAGON"):
+            print("You fight")
+            enemy("Red Dragon",1500,400)
+            rep+=300
+            hero_max_health+=70
+            hero_health[0]=hero_max_health
+            hero_max_power+=30
+            hero_power[0]=hero_max_power
+            print("You become more vicious for every red dragon you face")
+            print("you gain some reputation for the fight")
+            print("Totol Reputation: "+ str(rep))  
+            
+            break
+            
+
+        else:
+            print("Try again")
+            break
+
+    turn_counter+=1
+    print("It is turn "+str(turn_counter))
+
+    if rep>2000:
+        print("You are the master of the arena")
+        print("You beat the game in turn "+ str(turn_counter))
+        input("You win, press enter to exit")
+        break 
+    
+
+
+    
+
+
+   
+
+
+    
+    
