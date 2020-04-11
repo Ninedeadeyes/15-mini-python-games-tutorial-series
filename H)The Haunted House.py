29c17f9eae0aec4300cb6d83aec70a40523532c2
Ninedeadeyes <tommykwong_84@hotmail.com>
2020-04-11 20:13:15 +0100

    Add files via upload

--- H)The Haunted House.py
+++ H)The Haunted House.py
@@ -85,7 +85,7 @@ while True:
     
   move = move.lower().split()   # lower case everything and split will turn a string into a list hence  ' get shotgun' = move =[get,shotgun]
 
-  if move[0]=="shoot":    # acquire the value from index 0 of the dictionary of 'move'. In this case if it is shoot  
+  if move[0]=="shoot":    # acquire the value from index 0 of the list of 'move'. In this case if it is shoot  
       if "monster"in rooms[currentRoom] and move[1] in rooms[currentRoom]['monster'] and "shotgun" in inventory :
           del rooms[currentRoom]['monster']
           print( "you killed the", move[1])
