1fde283acca6419070d9245a1d96471f45479f37
Ninedeadeyes <tommykwong_84@hotmail.com>
2020-04-11 20:35:45 +0100

    Add files via upload

--- B) Hacker.py
+++ B) Hacker.py
@@ -1,10 +1,12 @@
 
 
 print("You are a hacker and attempting to hack Robert's bank account")
+print("                                                                   ")
 input("                 press enter to continue")
 print("                                                                   ")
 print("You know he is 24 years old and like cats and work as an accountant")
 print("                                                                   ")
+
 input("                 press enter to continue")
     
 print ("Welcome to Catwest Security System")
@@ -28,16 +30,17 @@ while password != 'bob':    # When password is 'bob' this will break
    if attempt == 3:
       print(" Tip(Begins with a 'B')")
 
-   
+   if attempt == 5:
+      print(" Tip( Relates to his name)")
 
-   if attempt == 7:
+   if attempt == 8:
        print("too many attempts")
        input("press enter to quit")
        quit()
 
    raw = input("Enter the Password ")
 
-   password=raw.lower()
+   password=raw.lower()     
        
    
 print("Access granted")
@@ -46,8 +49,14 @@ print("Access granted")
 input("press enter to exit")  # In theory you can click any button to exit 
 
 
+      # Examples of Python operators  
       
       #!= is not equal 
       # += add
+      # -= minus
       # == equals
+      # > Greater than
+      # < less than
+      # >= Greater than or equal to
+      # <= Less than or equal to 
       
