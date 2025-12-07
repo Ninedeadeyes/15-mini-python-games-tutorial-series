032202f76a2cf1ca1a3e538f65814c1bb61eed4c
Ninedeadeyes <tommykwong_84@hotmail.com>
2025-12-07 22:40:14 +0000

    Add files via upload

---
+++ I ) A Grots Life ( Version C )/OOP 4 ( pure oop and Modularity)/game_data.py
@@ -0,0 +1,8 @@
+class GameData:
+    def __init__(self):
+        self.lootlist = ["Fist Full of Straws","Fish Bone","Old Stick","Dirty Potato Sack","Crumbling Stones","Rusty Umbrella","Rope"]
+        self.enemylist = ("Rat","Hobbit","Tiny Dragon","Drunk Human","Killer Bee")
+        self.inventory = []
+        self.food = ("Mushrooms","Meaty Bones","Rotten Apples","Spoiled Meat")
+        self.larder = []
+        self.goal = []
